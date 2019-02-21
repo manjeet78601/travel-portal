7d95511cf18733f6a7094380ac7de7c88cdd915e
manjeet78601 <manjeetpathania13@gmail.com>
2019-02-21 11:29:16 +0530

    first commit

---
+++ signup.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<?php
+include("includes/header.php");
+?>
+<title>
+	Sign up Form 
+</title>
+</head>
+<body>
+	<div class="container">
+		<div class="login-form col-md-4 offset-md-4">
+			<div class="jumbotron" style="margin-top:20px;">
+		</div>
+		</div>
+	</div>
+</body>
